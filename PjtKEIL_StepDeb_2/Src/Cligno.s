1e2e6943233dd10a7a17f79f0121b71da9b8c2e4
Ger0th <75329725+Ger0th@users.noreply.github.com>
2021-04-17 17:18:56 +0200

    Add files via upload

---
+++ PjtKEIL_StepDeb_2/Src/Cligno.s
@@ -0,0 +1,77 @@
+	PRESERVE8
+	THUMB   
+		
+		
+;code C		
+;void timer_callback(void)
+;{
+;	if (FlagCligno==1)
+;	{
+;		FlagCligno=0;
+;		GPIOB_Set(1);
+;	}
+;	else
+;	{
+;		FlagCligno=1;
+;		GPIOB_Clear(1);
+;	}
+;		
+;}
+
+	include Driver/DriverJeuLaser.inc
+	export timer_callback
+	
+; ====================== zone de réservation de données,  ======================================
+;Section RAM (read only) :
+	area    mesdata,data,readonly
+
+
+;Section RAM (read write):
+	area    maram,data,readwrite
+		
+
+FlagCligno dcb 0	
+; ===============================================================================================
+	
+
+
+		
+;Section ROM code (read only) :		
+	area    moncode,code,readonly
+; écrire le code ici		
+	
+; Algo C
+;void Timer_callback(void)
+;{
+;	if (FlagCligno==1)
+;	{
+;		FlagCligno=0;
+;		GPIOB_Set(1);
+;	}
+;	else
+;	{
+;		FlagCligno=1;
+;		GPIOB_CLear(1);
+;	}
+;}
+
+timer_callback proc
+Si
+	ldr r1, =FlagCligno
+	ldrb r0, [r1]
+	adds r0, r0
+	beq Sinon
+Alors
+	mov r0, #0
+	strb r0, [r1]
+	mov r0, #1
+	bl GPIOB_Clear
+	b FinSi
+Sinon
+	mov r0, #1
+	strb r0, [r1]
+	bl GPIOB_Set
+FinSi
+	bx lr
+	endp
+	end	
